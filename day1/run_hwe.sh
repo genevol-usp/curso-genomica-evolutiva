3194e959f867381c376c24537b1c42683e530b55
VitorAguiar <vitor.aguiar@me.com>
2019-04-16 13:37:58 -0300

    calc hwe with vcftools

--- day1/run_hwe.sh
+++ day1/run_hwe.sh
@@ -7,3 +7,8 @@ YRIVCF=./data/chr22.yri.filtered.vcf.gz
 bcftools view --samples-file $SAMPLES $VCF |\
     bcftools view --min-af 0.05:minor --genotype ^miss -m2 -M2 --trim-alt-alleles \
     -o $YRIVCF -O z - 
+
+bcftools query -f '%ID %AN %AC{0}\n' $YRIVCF |\
+    awk '{printf "%s %f\n",$1,$3/$2}' > ./data/allele_freq.tsv
+
+vcftools --gzvcf $YRIVCF --out ./data/genos --hardy
