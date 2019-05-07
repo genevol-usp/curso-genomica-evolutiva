b9504b81ae7a7a3c9b221a8f68d816a477f46f0d
VitorAguiar <vitor.aguiar@me.com>
2019-05-07 16:29:04 -0300

    include allele freqs in drift_variable_size and make drift scripts consistent

--- dia2/drift_single_pop.R
+++ dia2/drift_single_pop.R
@@ -1,19 +1,19 @@
 # Genetic Drift
 
-# Number of individuals:
-N <- 1000
-
 # Number of generations
 ngens <- 2000
+
+# Number of individuals:
+N <- 1000
 
 # Population of individuals '0' or '1':
 pop <- rep(0:1, each = N/2)
 
 #vector with the frequencies of allele 0 in 2000 generations 
-p <- numeric(ngens)
+freqs <- numeric(ngens)
 
 # 1st element of vector 'p' will be the allele frequency in parent generation:
-p[1] <- mean(pop == 0)
+freqs[1] <- mean(pop == 0)
 
 # Now we have to calculate the allele frequencies in the next generations.
 # Let's use a 'for loop' to calculate the frequency in each generation and
@@ -24,10 +24,10 @@ for(i in 2:ngens) {
     pop <- sample(pop, replace = TRUE)
     
     # allele frequency in the ith generation:
-    p[i] <- mean(pop == 0)
+    freqs[i] <- mean(pop == 0)
 }
 
 # Plot
-plot(p, type = "l", ylim = c(0, 1),
+plot(freqs, type = "l", ylim = c(0, 1),
      xlab = "generations", ylab = "frequency", 
      main = "allele frequency change")
--- dia2/drift_variable_sizes.R
+++ dia2/drift_variable_sizes.R
@@ -1,20 +1,49 @@
-# vector of pop sizes
-sizes <- sample(1:100, 100)
+# Genetic Drift with variable population size
 
-ngens <- 100
+# Number of generations
+ngens <- 2000
 
-p <- numeric()
-p[1] <- 0.5
+# Number of individuals:
+N <- sample(10:1000, ngens, replace = TRUE)
+
+# Population of individuals '0' or '1':
+pop <- rep(0:1, each = N[1]/2)
+
+# Vector with the frequencies of allele 0 through the 'ngens' generations 
+freqs <- numeric(ngens)
+
+# 1st element of vector will be the allele frequency in parent generation 'pop':
+freqs[1] <- mean(pop == 0)
+
+# vector of heterozigosities through the 'ngens' generations
+het <- numeric(ngens)
+het[1] <- mean(pop == 0)
 
 for(i in 2:ngens) {
-    p[i] <- p[i - 1] * (1 - (1/(sizes[i - 1])))
+    
+    # the next generation is a random sample of the parental population:
+    pop <- sample(pop, size = N[i], replace = TRUE)
+    
+    # allele frequency in the ith generation:
+    freqs[i] <- mean(pop == 0)
+    
+    # heterozigosity in the ith generation
+    het[i] <- het[i - 1] * (1 - (1/(N[i - 1])))
 }
 
-plot(p, type = "l", ylim = c(0, 1),
+plot(freqs, type = "l", ylim = c(0, 1),
      xlab = "generations", ylab = "frequency", 
      main = "allele frequency change")
 
-ne <- (sum(sizes^-1) / length(sizes)) ^-1
-H_t <- p[1] * (1 - (1/ne))^(1:ngens)
+plot(het, type = "l", ylim = c(0, 1),
+     xlab = "generations", ylab = "H", 
+     main = "Heterozigosity change")
 
+# Ne
+ne <- (sum(N^-1) / length(N)) ^-1
+
+# Theoretical H change
+H_t <- het[1] * (1 - (1/ne))^(1:ngens)
+
+# add theoretical line to the plot of H
 lines(H_t, col = "red", lty = 2)
