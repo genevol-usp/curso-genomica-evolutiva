43aebdfae1c995e35451a94b5aff4ad8f73c5b47
VitorAguiar <vitor.aguiar@me.com>
2019-05-13 11:44:49 -0300

    add file

---
+++ dia4/launch_jobs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#PBS -l nodes=1:ppn=1
+#PBS -l mem=8gb
+#PBS -l walltime=24:00:00
+#PBS -q short
+#PBS -t 1-3 
+#PBS -j oe
+#PBS -o $PBS_JOBID.log
+
+cd $PBS_O_WORKDIR
+
+pop=`awk "FNR==$PBS_ARRAYID" ./populations.txt`
+
+vcftools --vcf ./dados/SNPs_Chr2_${pop}_maf.recode.vcf --chr 2 --TajimaD 1000 --out $pop
