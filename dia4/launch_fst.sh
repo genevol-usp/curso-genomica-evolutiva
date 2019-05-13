31e29f24f0e3c8bbd1a0695434599ed7269b1cd4
VitorAguiar <vitor.aguiar@me.com>
2019-05-13 14:00:31 -0300

    add files

---
+++ dia4/launch_fst.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#PBS -l nodes=1:ppn=1
+#PBS -l mem=8gb
+#PBS -l walltime=24:00:00
+#PBS -q short
+#PBS -t 1-3 
+#PBS -j oe
+#PBS -o $PBS_JOBID.log
+
+cd $PBS_O_WORKDIR
+
+pair=$(awk "FNR==$PBS_ARRAYID" ./population_pairs.txt)
+
+pop1=$(echo $pair | cut -d'_' -f1)
+pop2=$(echo $pair | cut -d'_' -f2)
+
+/home/debora/vcftools/src/cpp/vcftools --vcf ./dados/SNPs_Chr2_AFR_EUR_EAS_maf.recode.vcf \
+    --out ${pair}_maf --chr 2 \
+    --weir-fst-pop ./dados/pop_${pop1}_1000g.txt \
+    --weir-fst-pop ./dados/pop_${pop2}_1000g.txt
