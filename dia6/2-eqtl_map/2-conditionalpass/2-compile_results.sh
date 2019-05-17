1b74cf0717fcc6addcaebd53ad27f6cac26ba824
VitorAguiar <vitor.aguiar@me.com>
2019-05-17 01:41:30 -0300

    update

--- dia6/2-eqtl_map/2-conditionalpass/2-compile_results.sh
+++ dia6/2-eqtl_map/2-conditionalpass/2-compile_results.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pc=50
-out=./conditional_$pc
+pc=20
+out=/scratch/bio5789/conditionalpass/conditional_$pc
 
 cat ${out}_{1..64}.txt | gzip -c > ${out}_all.txt.gz && rm ${out}_{1..64}.txt 
